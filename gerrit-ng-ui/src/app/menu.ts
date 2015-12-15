878ad7c181e3f115c53231e549eac43c0ee4d7bd
joshuawilson <javajoshw@gmail.com>
2015-12-14 21:57:47 -0500

    Add initial stucture for Menu componant. Add Eclipse.svg to be used in Header.

---
+++ gerrit-ng-ui/src/app/menu.ts
@@ -0,0 +1,27 @@
+/**
+ * Created by josh on 12/14/15.
+ */
+/**
+ * Created by josh on 12/12/15.
+ */
+import {bootstrap, Component} from 'angular2/angular2';
+
+
+@Component({
+    selector: 'menu',
+    template:`
+        <div (click)='onClick()'>
+            {{menuName}}
+        </div>
+    `,
+    directives:[]
+})
+
+class MenuComponent {
+    public menuName: search;
+    public onClick(){
+        alert('add code')
+    }
+}
+
+bootstrap(MenuComponent);
