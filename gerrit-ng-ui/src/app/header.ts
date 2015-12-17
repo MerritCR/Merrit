7ea5a911b174c687c7e0925300fc8bb1432b8a0a
joshuawilson <javajoshw@gmail.com>
2015-12-16 19:48:44 -0500

    Connect menu to header.

--- gerrit-ng-ui/src/app/header.ts
+++ gerrit-ng-ui/src/app/header.ts
@@ -1,3 +1,19 @@
 /**
  * Created by josh on 12/12/15.
  */
+import {bootstrap, Component, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
+import {menu} from './menu'
+
+@Component({
+    selector: 'header',
+    template:`
+        <menu></menu>
+    `,
+    directives:[CORE_DIRECTIVES, FORM_DIRECTIVES, menu]
+})
+
+class AppComponent {
+
+}
+
+bootstrap(AppComponent);
--- gerrit-ng-ui/src/app/menu.ts
+++ gerrit-ng-ui/src/app/menu.ts
@@ -6,7 +6,6 @@
  */
 import {bootstrap, Component} from 'angular2/angular2';
 
-
 @Component({
     selector: 'menu',
     template:`
