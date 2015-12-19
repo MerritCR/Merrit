1eb23a3b0f85d427f26442e881b5be052ebefc58
joshuawilson <javajoshw@gmail.com>
2015-12-18 23:11:58 -0500

    Update app to use Angular 2 modular structure.

--- gerrit-ng-ui/src/app/app.ts
+++ gerrit-ng-ui/src/app/app.ts
@@ -1,4 +1,4 @@
-import {bootstrap, Component, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
+import {Component} from 'angular2/angular2';
 import {header} from './header'
 
 @Component({
@@ -7,11 +7,11 @@ import {header} from './header'
         <h1>Main page for Gerrit review.</h1>
         <header></header>
     `,
-    directives:[CORE_DIRECTIVES, FORM_DIRECTIVES, header]
+    directives:[header]
 })
 
-class AppComponent {
+export class AppComponent {
 
 }
 
-bootstrap(AppComponent);
+
